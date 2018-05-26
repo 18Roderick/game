266700409cb15fe449495483752c0777ea4f95e8
18Roderick <rjrr507@gmail.com>
2018-05-25 21:24:41 -0600

    Archivos modificados

--- models/connection.php
+++ models/connection.php
@@ -1,18 +1,17 @@
-<?php 
-	include 'config.php';
-	/**
-	* 
-	*/
-	class Connection 
-	{
-		protected $db;
-		function __construct()
-		{
-			$this->db = new mysqli(DB_HOST, DB_USER, PASSWORD, DB_NAME);
-			if ($this->db->connect_errno) {
-				echo "Fallo al conectar la base de datos".$this->db->connect_errno;
-			}
-			
-		}
-	}
- ?>
+<?php
+require 'config.php';
+/**
+Conexion  abase de datos
+ */
+class Connection
+{
+    protected $db;
+    public function __construct()
+    {
+        $this->db = new mysqli(DB_HOST, DB_USER, PASSWORD, DB_NAME);
+        if ($this->db->connect_errno) {
+            echo "Fallo al conectar la base de datos" . $this->db->connect_errno;
+        }
+
+    }
+}
--- models/usuarios.php
+++ models/usuarios.php
@@ -1,37 +1,38 @@
-<?php 
-include('connection.php');
+<?php
 
-class Usuarios extends Connection{
-  #$db bariable que contiene la conexion a la base de datos
-  public function __construct(){
-		parent::__construct();
-  }
-	public function nuevo_usuario($nombre,$apellido,$sexo, $fecha, $username, $correo, $password){
+require 'connection.php';
 
-		$instruccion = "CALL nuevo_usuario('".$nombre."','".$apellido."','".$sexo."','".$fecha."'
-											,'".$username."','".$correo."','".$password."')";
+class Usuarios extends Connection
+{
+    // A $db bariable que contiene la conexion a la base de datos
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    public function nuevo_usuario($nombre, $apellido, $sexo, $fecha, $username, $correo, $password)
+    {
 
-		$consulta = $this->db->query($instruccion);		
-		$this->db->close();
-		
-	}
+        $instruccion = "CALL nuevo_usuario('" . $nombre . "','" . $apellido . "','" . $sexo . "','" . $fecha . "'
+											,'" . $username . "','" . $correo . "','" . $password . "')";
 
+        $consulta = $this->db->query($instruccion);
+        $this->db->close();
 
-  public function existe_usuario($correo){
+    }
 
-      $instruccion = "CALL existe_usuario('".$correo."')";
+    public function existe_usuario($correo)
+    {
 
-      $consulta = $this->db->query($instruccion);
-      $resultado = $consulta->fetch_all(MYSQLI_ASSOC);
+        $instruccion = "CALL existe_usuario('" . $correo . "')";
 
-      if ($resultado) {
-        return $resultado;        
-      }
-      
-      $this->db->close();
-    }  
+        $consulta = $this->db->query($instruccion);
+        $resultado = $consulta->fetch_all(MYSQLI_ASSOC);
 
+        if ($resultado) {
+            return $resultado;
+        }
 
+        $this->db->close();
+    }
 
 }
-?>
