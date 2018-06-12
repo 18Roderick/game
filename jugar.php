675ab3df47894bfbc8d9648f343c34ae3a8ccb81
18Roderick <rjrr507@gmail.com>
2018-06-12 00:19:45 -0500

    redireccionamiento agregado

--- jugar.php
+++ jugar.php
@@ -1,4 +1,9 @@
 <?php 
-include 'header.php'; 
-include 'vistas/box.php';
+if($_SESSION['usuario']){
+  include 'header.php'; 
+  include 'vistas/box.php';
+}else{
+  header('Location: http://localhost/game');
+}
+
 ?>
