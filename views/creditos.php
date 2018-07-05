38217aee46ae15cf3d01c944645ac60d7fa203e3
Aldair <duquealdair@gmail.com>
2018-07-05 00:51:20 -0500

    creditos actualizados

--- views/creditos.php
+++ views/creditos.php
@@ -10,40 +10,24 @@ print('
 
 ?>
 
- <div class="container">
-<!--   /*<div class="row">
-    <div class="col s12 m6">
-      <div class="card">
-        <div class="card-image">
-          <img src="imagenes/aldair20.jpg"class="col s12 m6" >
-          <span class="card-title">Aldair Duque</span>
-          <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
-        </div>
-        <div class="card-content">
-          <p>Cedula: 8-923-2304 
-          Edad: 21 
-          Universidad: Universidad Tecnologica de Panama.
-          Bla bla bla
-          .</p>
-        </div>
-      </div>
-    </div>
-    </div> -->
+<!--<div class="container">-->
 
     <div class="row">
 
     <div class="col s12 m4">
       <div class="card">
         <div class="card-image">
-          <img src="https://materializecss.com/images/sample-1.jpg">
-          <span class="card-title">Card Title</span>
+          <img src="imagenes/aldair20.jpg">
+          <span class="card-title">Aldair Duque</span>
         </div>
         <div class="card-content">
-          <p>I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively.</p>
+          <p>Edad: 21 años </br>
+           Correo: duquealdair@gmail.com</b>
+           Carrera: Licenciatura en Desarrollo de Software.
+          </p>
         </div>
         <div class="card-action">
-          <a href="#">This is a link</a>
+          <a href="https://github.com/neaxx">Github</a>
         </div>
       </div>
     </div>
@@ -51,34 +35,81 @@ print('
         <div class="col s12 m4">
       <div class="card">
         <div class="card-image">
-          <img src="https://materializecss.com/images/sample-1.jpg">
-          <span class="card-title">Card Title</span>
+          <img src="imagenes/roderick.jpeg">
+          <span class="card-title">Roderick Romero</span>
         </div>
         <div class="card-content">
-          <p>I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively.</p>
+          <p>Edad: 21 años</br>
+          Correo: rjrr507@gmail.com</br>
+          Carrera: Licenciatura en Desarrollo de Software.
+          </p>
         </div>
         <div class="card-action">
-          <a href="#">This is a link</a>
-        </div>
-      </div>
-    </div>
-    <div class="col s12 m4">
-      <div class="card">
-        <div class="card-image">
-          <img src="https://materializecss.com/images/sample-1.jpg">
-          <span class="card-title">Card Title</span>
-        </div>
-        <div class="card-content">
-          <p>I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively.</p>
-        </div>
-        <div class="card-action">
-          <a href="#">This is a link</a>
+          <a href="https://github.com/18Roderick">Github</a>
         </div>
       </div>
     </div>
 
-  </div>
-          
+
+        <div class="col s12 m4">
+      <div class="card">
+        <div class="card-image">
+          <img src="imagenes/aldhier.jpg">
+          <span class="card-title">Aldhier Torres</span>
+        </div>
+        <div class="card-content">
+          <p>Edad: 22 años</br>
+          Correo: aldhier03@gmail.com</br>
+          Carrera: Licenciatura en Desarrollo de Software.
+          </p>
+        </div>
+        <div class="card-action">
+          <a href="#">Github</a>
+        </div>
+      </div>
     </div>
+
+   </div><!--row div 1 -->
+
+   <div class="row">
+     
+    <div class="col s12 m4">
+      <div class="card">
+        <div class="card-image">
+          <img src="imagenes/Jonhy.jpg">
+          <span class="card-title">Juan Samudio</span>
+        </div>
+        <div class="card-content">
+          <p>Edad: 23 años</br>
+          Correo: jhonysan-0100@hotmail.com</br>
+          Carrera: Licenciatura en Desarrollo de Software.
+          </p>
+        </div>
+        <div class="card-action">
+          <a href="#">Github</a>
+        </div>
+      </div>
+    </div>
+
+    
+    <div class="col s12 m4">
+      <div class="card">
+        <div class="card-image">
+          <img src="imagenes/deivy.jpeg">
+          <span class="card-title">Deyvi Morales</span>
+        </div>
+        <div class="card-content">
+          <p>Edad: 24 años</br>
+          Correo:deivis93@live.com</br>
+          Carrera: Licenciatura en Desarrollo de Software.
+          </p>
+        </div>
+        <div class="card-action">
+          <a href="https://github.com/Deyvimh">Github</a>
+        </div>
+      </div>
+    </div>
+          
+    </div><!--row div 2-->
+
+<!--</div>--> <!--container div --> 
